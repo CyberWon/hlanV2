c5011e142c1d1d4edc4c9abe11a63b3a7db874e0
shuaibo.wang <mail@shuaibo.wang>
2016-04-27 16:37:51 +0800

    v2.0.3

--- mod/mod_outgen.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-#coding:utf-8
-def out(gen):
-    for i in gen:
-        print i
--- mod/test.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-#coding:utf-8
-def mod_main(arvgs):
-    yield False
-    print 'test'    
